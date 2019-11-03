f97090c04f2a7529f840d23ec0eadb225f5e4428
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-03 02:55:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@
 var playlist = { kanyeWest: 'Lift Yourself'}
 
 function updatePlaylist(playlist,artistName,songTitle) {
-// var newPlaylist = {}
+
  Object.assign(playlist,{[artistName]: songTitle})
  return playlist
 }
